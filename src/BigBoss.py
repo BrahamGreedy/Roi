ab8eec7ffdc8f0e8ebd6b0eab9708ccbbe2e96b0
BrahamGreedy <braham.2020@mail.ru>
2025-12-09 11:24:33 +0500

    Add raw BigBoss.py

---
+++ src/BigBoss.py
@@ -0,0 +1,18 @@
+class BigBoss():
+    def __init__(self):
+        pass
+
+    def get_command(self):
+        pass
+
+    def send_command(self):
+        pass
+
+    def get_frame(self):
+        pass
+
+    def get_agent_positions(self):
+        pass
+
+    def create_logic(self):
+        pass
