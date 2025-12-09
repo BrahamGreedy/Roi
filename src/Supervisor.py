55f865c7b451a8f48bdd4954db560c497c45d0ea
BrahamGreedy <braham.2020@mail.ru>
2025-12-09 14:08:56 +0500

    Add raw Supervisor.py

---
+++ src/Supervisor.py
@@ -0,0 +1,25 @@
+import cv2
+
+class Supervisor():
+    def __init__(self):
+        pass
+
+    def aruco_markers_detect(self):
+        '''
+        детекция aruco маркеров используя opencv
+        '''
+        pass
+
+    def get_agents_rotate_angle(self):
+        pass
+
+    def get_field(self):
+        '''
+        тут будем получать кроп фрейма
+        '''
+        pass
+
+    def get_frame(self):
+        pass
+
+    
