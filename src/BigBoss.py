2f3b2853d24e2a61f901a76076e69cd884b717d4
BrahamGreedy <braham.2020@mail.ru>
2025-12-10 12:54:50 +0500

    Add connect with Supervisor

--- src/BigBoss.py
+++ src/BigBoss.py
@@ -1,14 +1,22 @@
+from Supervisor import Supervisor
+
 class BigBoss():
     def __init__(self):
-        pass
+        self.supervisor = Supervisor()
+
+    def get_field_size(self):
+        self.field_size = self.supervisor.get_field_size()
+
+    def get_robot_data(self):
+        self.robot_data = self.supervisor.get_robot_data()
+    
+    def get_breakstone_mask(self):
+        self.breakstone_mask = self.supervisor.get_breakstone_mask()
 
     def get_command(self):
         pass
 
     def send_command(self):
-        pass
-
-    def get_frame(self):
         pass
 
     def get_agent_positions(self):
